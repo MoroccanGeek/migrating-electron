73b4081afedfc4f4310b29333c63249205209aa7
Hamza SABER <saberhamza3@gmail.com>
2020-10-09 10:24:30 +0100

    Add Python scripts to assets folder

---
+++ src/assets/pyscripts/python-sqlite-test.py
@@ -0,0 +1,141 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_project(conn, project):
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+def create_task(conn, task):
+
+    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+    return cur.lastrowid
+
+def update_task(conn, task):
+    sql = ''' UPDATE tasks
+              SET priority = ? ,
+                  begin_date = ? ,
+                  end_date = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+def delete_task(conn, id):
+    sql = 'DELETE FROM tasks WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
+
+def delete_all_tasks(conn):
+    sql = 'DELETE FROM tasks'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+def main_create_tables():
+
+    database = "src/assets/data/database.sqlite"
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+        
+        conn.close()
+    else:
+        print("Error! cannot create the database connection.")
+
+def main_Insert():
+    database = "src/assets/data/database.sqlite"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        # create a new project
+        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
+        project_id = create_project(conn, project)
+
+        # tasks
+        task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
+        task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
+
+        # create tasks
+        create_task(conn, task_1)
+        create_task(conn, task_2)
+
+def main_update():
+    database = "src/assets/data/database.sqlite"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        update_task(conn, (2, '2015-01-04', '2015-01-06', 2))
+
+def main_delete():
+    database = "src/assets/data/database.sqlite"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        delete_task(conn, 2);
+        # delete_all_tasks(conn);
+
+if __name__ == '__main__':
+    main_Insert()
+    print(300)

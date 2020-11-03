bd9879645a4468bf838ebb4677e906565f039005
Hamza SABER <saberhamza3@gmail.com>
2020-11-03 15:36:04 +0100

    Add app configuration module

---
+++ src/app/modules/app-config/app-config.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppConfigRoutingModule } from './app-config-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppConfigRoutingModule
+  ]
+})
+export class AppConfigModule { }

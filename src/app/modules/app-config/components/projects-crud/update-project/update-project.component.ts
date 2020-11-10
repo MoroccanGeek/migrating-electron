bc84f493d2c76e56009b3087c0a64046e8b7ba2e
Hamza SABER <saberhamza3@gmail.com>
2020-11-10 13:32:34 +0100

    Projects management - Update Project

--- src/app/modules/app-config/components/projects-crud/update-project/update-project.component.ts
+++ src/app/modules/app-config/components/projects-crud/update-project/update-project.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Project } from '@assets/models/project.entity';
+import { Account } from '@assets/models/account.entity';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AccountService, ProjectService } from '@core/services';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-update-project',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProjectComponent implements OnInit {
 
-  constructor() { }
+  UpdateProjectForm: FormGroup;
+  event: EventEmitter<any> = new EventEmitter();
+  accountsList: Account[];
+  tempProject: Project;
+
+  constructor(private builder: FormBuilder, private accountService: AccountService ,private projectService: ProjectService, private bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
+    this.UpdateProjectForm = this.builder.group({
+      accounts: new FormControl(null),
+      name: new FormControl(this.tempProject.name, []),
+    });
+
+    this.accountService.getAccounts().subscribe((accounts: any) => {
+      this.accountsList = accounts;
+    });
+
+    // this is to dynamically select the Account of the given API key
+    this.UpdateProjectForm.controls['accounts'].setValue(this.tempProject.account.id,{onlySelf: true});
+  }
+
+  onProjectUpdateFormSubmit() {
+
+    // let tempProject = new Project();
+
+    this.tempProject.name = this.UpdateProjectForm.get('name').value;
+
+    this.accountService.getAccountById(this.UpdateProjectForm.get('accounts').value).subscribe(account => {
+      this.tempProject.account = account[0];
+
+      this.projectService.updateProject(this.tempProject).subscribe( projects => {
+      
+        if(projects!=null && projects.length>0){
+          let response = {
+            'response': 'OK',
+            'data': projects
+          }
+          this.event.emit(response);
+          this.bsModalRef.hide();
+        }
+      });
+      
+    });
+
+  }
+
+  onClose(){
+    this.bsModalRef.hide();
   }
 
 }
